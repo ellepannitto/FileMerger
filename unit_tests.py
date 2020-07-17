e2e8b45e3f9248789abc3197a000e440ab15d910
Lucio Messina <luciotuttominuscolo@inventati.org>
2020-07-17 20:07:27 +0200

    unit tests for base merge_and_collapse and automatic filename chosing

--- unit_tests.py
+++ unit_tests.py
@@ -1,18 +1,30 @@
 import unittest
 import gzip
+import os
 
 from filesmerger import merge_and_collapse_pattern, Mode
 
 class TestFileMerger (unittest.TestCase):
-        
+    
+    def _check_expected_values ( self, fin ):
+        expected_values = {"a": 33, "b": 16, "c":40, "d":19, "e":8, "f":10, "g":9}
+        for line in fin:
+            line = line.strip().split()
+            key, val = line[0], float(line[1])
+            self.assertEqual (val, expected_values[key], "value for {} is {}. should be {}".format(key, val, expected_values[key]))
+
     def test_merge_and_collapse (self):
         merge_and_collapse_pattern ("data/*.txt", "tmp_output")
-        expected_values = {"a": 33, "b": 16, "c":40, "d":19, "e":8, "f":10, "g":9}
+        self.assertTrue (os.path.isfile("tmp_output"))
         with open ("tmp_output") as fin:
-            for line in fin:
-                line = line.strip().split()
-                key, val = line[0], float(line[1])
-                self.assertEqual (val, expected_values[key], "value for {} is {}. should be {}".format(key, val, expected_values[key]))
+            self._check_expected_values (fin)    
+        os.remove("tmp_output")
+
+    def test_automatic_output_file_naming (self):
+        output_path = merge_and_collapse_pattern ("data/*.txt")
+        with open (output_path) as fin:
+            self._check_expected_values (fin)
+        os.remove(output_path)
 
     # def test_merge_and_collapse_gz ( self ):
     #     merge_and_collapse_pattern ("data/*.gz", "tmp_output2.gz", mode=Mode.gzip)
